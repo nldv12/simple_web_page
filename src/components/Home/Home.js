7037275277e569d9b259f2690bd4d37225994399
Nazar Majewski <nldv12@gmail.com>
2021-11-21 09:16:05 +0100

    home_ready

---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.scss"
+
+export const Home = (props) => {
+
+    return (
+        <div className={"Home"}>
+            <div className={"box"}>
+            <div className={"title text"}>Lorem ipsum dolor.</div>
+            </div>
+            <div className={"box"}>
+                <div className={"text"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam aspernatur at atque beatae debitis dicta dolorum ea earum eius ex in ipsam minus modi necessitatibus omnis, perspiciatis quo quos, ratione reiciendis repellat rerum tempora ut velit voluptatum. Aspernatur enim fugiat molestiae odit officiis omnis placeat quibusdam quo, temporibus velit? Ad corporis dolores minima repellat. Alias, aliquid, asperiores beatae commodi consectetur consequatur corporis delectus dolor ea fuga illum impedit magni nam nisi quam quibusdam quod reprehenderit sapiente soluta tempore unde.</div>
+            </div>
+            <div className={"author_box box"}>
+                <div className={"text"}>Aleksander Kwaśniewski</div>
+                <div className={"text"}>15 listopada 1954</div>
+            </div>
+
+        </div>
+        )
+}
